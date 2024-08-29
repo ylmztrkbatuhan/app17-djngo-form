78551f343f05765489ff08fb404e3684d2cbabcb
oboy21 <oboy11b@gmail.com>
2024-08-29 13:40:21 +0300

    prt 2

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import  ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 def index(request):
     if request.method == "POST":
         form = ApplicationForm(request.POST)
@@ -9,7 +12,12 @@ def index(request):
             email =form.cleaned_data["email"]
             date =form.cleaned_data["date"]
             occupation =form.cleaned_data["occupation"]
-            print(first_name)
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
 
+            message_body = f"A new job application was submitted. Thank you, \n{first_name}"
+            email_messsage = EmailMessage("Form submission confirmation", message_body, to=[email])
+            email_messsage.send()
+        messages.success(request, "Form submitted successfully!")
     return render(request,"index.html")
 
